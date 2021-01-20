c389cb8144fd096676cdddd73cddf18b4a3e25e6
Franco Fusco <francofusco93@gmail.com>
2021-01-20 19:37:14 +0100

    Added my workspace

--- config-ros1.bash
+++ config-ros1.bash
@@ -1,6 +1,13 @@
 # source the "main" setup.bash file of the ROS1 distribution we want to use.
 source /opt/ros/noetic/setup.bash
 
+# ROS_HOME is where I store all my catkin workspaces
+export ROS_HOME=$HOME/programming/ROS
+alias roshome="cd $ROS_HOME"
+
+# source my personal workspaces
+source $ROS_HOME/devel/devel/setup.bash
+
 # Finally, make sure that every time we open a console, we are greeted with a
 # message informing us about the version we are using
 echo "Working with ROS1 $ROS_DISTRO"
