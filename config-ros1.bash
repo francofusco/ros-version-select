1d9b04a525d1a3357fa784b7b9e8f46cdc999dd1
Franco Fusco <francofusco93@gmail.com>
2021-01-21 17:18:18 +0100

    Changed ROS_HOME into ROS_WS_ROOT

--- config-ros1.bash
+++ config-ros1.bash
@@ -1,12 +1,12 @@
 # source the "main" setup.bash file of the ROS1 distribution we want to use.
 source /opt/ros/noetic/setup.bash
 
-# ROS_HOME is where I store all my catkin workspaces
-export ROS_HOME=$HOME/programming/ROS
-alias roshome="cd $ROS_HOME"
+# ROS_WS_ROOT is where I store all my catkin workspaces
+export ROS_WS_ROOT=$HOME/programming/ROS
+alias roshome="cd $ROS_WS_ROOT"
 
 # source my personal workspaces
-source $ROS_HOME/commons/devel/setup.bash
+source $ROS_WS_ROOT/commons/devel/setup.bash
 
 # Finally, make sure that every time we open a console, we are greeted with a
 # message informing us about the version we are using
